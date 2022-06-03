d7c6e21cdb23378a6b788508315c3483baf833ef
K K <kulacoglukoral@gmail.com>
2022-06-03 19:35:34 -0400

    added tests

--- Code/heatonDigit.py
+++ Code/heatonDigit.py
@@ -1,82 +1,38 @@
 import numpy as np
-import pandas as pd
-import tensorflow as tf
-from tensorflow.keras import layers
 from keras.datasets.mnist import load_data
-from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
 
-seed = 42
-np.random.seed(seed)
-tf.random.set_seed(seed)
+from Models.MLP import *
 
-def fetch_data():
+def get_data():
     (X, Y), (Xtest, Ytest) = load_data()
     dimensions = X.shape[1] * X.shape[2]
 
     X = X.reshape((X.shape[0], dimensions)) / 255.0
     Xtest = Xtest.reshape((Xtest.shape[0], dimensions)) / 255.0
 
-    Y = tf.keras.utils.to_categorical(Y)
-    Ytest = tf.keras.utils.to_categorical(Ytest)
+    (X, Y), (Xval, Yval) = split(X, Y, train_frac=0.9)
 
-    X, Xval, Y, Yval = train_test_split(X, Y, test_size=0.1, random_state=42)
+    Y = to_categorical(Y)
 
     return X, Y, Xtest, Ytest, Xval, Yval
 
-def heaton(i, o):
-    return int((2/3)*i+o)
+def heaton(I, O):
+    return round((2/3)*I + O)
 
-X, Y, Xtest, Ytest, Xval, Yval = fetch_data()
+X, Y, Xtest, Ytest, Xval, Yval = get_data()
 
-print('X-dims:', X.shape)
-print('Y-dims:', Y.shape)
+hidden_cnt = heaton(X.shape[1], Y.shape[1])
 
-neurons = heaton(X.shape[1], Y.shape[1])
-print('neurons:', neurons)
+layers = [
+    InputLayer(X.shape[1]),
+    Layer(hidden_cnt, ReLU()),
+    Layer(Y.shape[1], Softmax())
+]
 
-model = tf.keras.Sequential([
-    layers.Dense(neurons, input_shape=(X.shape[1],), activation='relu'),
-    layers.Dense(Y.shape[1], activation='softmax')
-])
+model = NeuralNetwork(layers)
+model.compile(loss_function=SquaredLoss(), metric=accuracy)
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics='accuracy')
+history = model.fit(X, Y, Xval, Yval, learning_rate=0.01, n_epochs=10, batch_size=32)
 
-history = model.fit(X, Y, validation_data=(Xval, Yval), epochs=10)
-
-training = pd.DataFrame(history.history)
-
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model Accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['training', 'validation'], loc='upper left')
-plt.show()
-
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model Loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['training', 'validation'], loc='upper left')
-plt.show()
-
-class_names = list(range(10))
-
-realY = np.argmax(Ytest.copy(), axis=1)
-predY = np.argmax(model.predict(Xtest), axis=1)
-
-cf_matrix = confusion_matrix(realY, predY)
-ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues')
-
-ax.set_title('Confusion Matrix\n')
-ax.set_xlabel('\nPredicted Values')
-ax.set_ylabel('\nActual Values')
-
-ax.xaxis.set_ticklabels(class_names)
-ax.yaxis.set_ticklabels(class_names, rotation=0)
-
-plt.show()
+model.print_predict(Xtest, Ytest)
+plot_loss_and_accuracy(history)
